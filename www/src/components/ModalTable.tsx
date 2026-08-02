d10aabdfd6b4a26f29f6f044031931849c36ff31
jespbel <jmbelaso@buffalo.edu>
2026-08-02 15:58:00 -0400

    Updated funciton so it now can update to pocketbase correctly

--- www/src/components/ModalTable.tsx
+++ www/src/components/ModalTable.tsx
@@ -13,29 +13,37 @@ export default function ModalTable( { members }: { members: Array<Record<string,
     <div id="myModal" className="modal">
       <div className="modal-content">
         <span className="close" onClick={closeModal}>&times;</span>
+        <div className="modal-table-wrapper">
         <table>
-          <thead>
+        <thead>
             <tr>
-              <th>Box ID</th>
-              <th>Status</th>
-              <th>Members</th>
-              <th>Waitlist</th>
-              <th>Updated By</th>
-              <th>Notes</th>
-            </tr>
+                <th colSpan={9}>Personal Info</th>
+                <th colSpan={9}>Member Status</th>
+             </tr>
           </thead>
+          <thead>
+                    <tr>
+                    <th>Full Name</th>
+                    <th>Amount Paid</th>
+                    <th>Member Role</th>
+                    <th>Member Type</th>
+                    <th>Member Status</th>
+                    <th>Email</th>
+                    <th>Street</th>
+                    <th>Status</th>
+                    <th>Zip Code</th>
+                    <th>Payment Status</th>
+                    <th>Amount Paid</th>
+                    <th>Payment Type</th>
+                    <th>Meetings Completed</th>
+                    <th>Servince Hours Required</th>
+                    </tr>
+                </thead>
           <tbody>
-            {members.map((box) => (
-              <tr key={box.id}>
-                <Checkbox  />
-                <td>{box.id}</td>
-                <td>
-                  <span className="badge">{box.box_state ?? "—"}</span>
-                </td>
-              </tr>
-            ))}
+            
           </tbody>
         </table>
+        </div>
       </div>
 
     </div>
